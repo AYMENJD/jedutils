7a6a8067bbd4ff10eaa71c6a3804d4af1c04df49
AYMENJD <53928879+AYMENJD@users.noreply.github.com>
2023-08-23 06:25:15 +0300

    Add math.format_duration.

--- jedutils/math/_number_formatter.py
+++ jedutils/math/_number_formatter.py
@@ -1,4 +1,4 @@
-def format_bytes(size):
+def format_bytes(size: int):
     """
     Converts a size in bytes to a human-readable format
 
@@ -20,7 +20,7 @@ def format_bytes(size):
     return f"{size:.2f} {suffixes[i]}"
 
 
-def parse_bytes(size_str):
+def parse_bytes(size_str: str):
     """
     Converts a size in a human-readable format to bytes
 
@@ -43,13 +43,79 @@ def parse_bytes(size_str):
     size, suffix = size_str[:-2], size_str[-2:].upper()
 
     if suffix not in suffixes:
-        raise ValueError("Invalid size format.")
+        raise ValueError("Invalid size format")
 
     try:
         size = float(size)
     except ValueError:
-        raise ValueError("Invalid size format.")
+        raise ValueError("Invalid size format")
 
     power = suffixes.index(suffix)
     size *= 1024**power
     return int(size)
+
+
+units = [
+    ("year", 365 * 24 * 60 * 60),
+    ("day", 24 * 60 * 60),
+    ("hour", 60 * 60),
+    ("minute", 60),
+    ("second", 1),
+]
+
+
+def format_duration(seconds: int, format_option: str = "short"):
+    """
+    Formats the given duration in seconds into a human-readable string representation
+
+    Args:
+        seconds (``int``):
+            The duration in seconds
+
+        format_option (``str``, *optional*):
+            The formatting option either ``long`` or ``short``. Default is "short"
+
+    Returns:
+        ``str``:
+            The formatted duration string
+
+    Raises:
+        ``ValueError``:
+            If the duration is a negative number
+    """
+
+    if seconds < 0:
+        raise ValueError("Duration must be a non-negative number")
+
+    parts = []
+    for unit, duration in units:
+        if seconds >= duration:
+            count = seconds // duration
+            seconds %= duration
+
+            if count > 1:
+                unit += "s"
+
+            parts.append(f"{count} {unit}")
+
+    if not parts:
+        return "0 seconds"
+
+    if format_option == "long":
+        if len(parts) == 1:
+            return parts[0]
+        last_part = parts.pop()
+        return ", ".join(parts) + " and " + last_part
+    elif format_option == "short":
+        hours = int(seconds // 3600)
+        minutes = int((seconds // 60) % 60)
+        seconds = int(seconds % 60)
+
+        time_parts = []
+        if hours > 0:
+            time_parts.append(str(hours))
+        time_parts.append(f"{minutes:02d}")
+        time_parts.append(f"{seconds:02d}")
+        return ":".join(time_parts)
+    else:
+        raise ValueError("Invalid format option. Available options: 'long', 'short'")
